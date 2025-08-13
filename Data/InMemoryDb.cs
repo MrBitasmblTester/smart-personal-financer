0d32bc08e6d9cae81c53e07b0ac2c466a94e04ad
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-08-13 13:39:54 +0400

    Add InMemoryDb.cs

---
+++ Data/InMemoryDb.cs
@@ -0,0 +1,4 @@
+using Project_Backend_2024.DTO.TechStack;
+using SmartPersonalFinancer.Models;
+namespace SmartPersonalFinancer.Data;
+public static class InMemoryDb{ public static readonly List<Expense> Expenses = new(); }
